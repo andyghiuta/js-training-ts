9910a7dc1d81fa4eb8fd1f45c49a813ac2195ffd
Andy Ghiuta <aghiuta@ena.com>
2020-06-28 11:17:40 +0300

    Session 1 coding challenges

---
+++ tests/session1/2.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import minMax from '../../session1/coding-challenges/2';
+
+describe('Session 1', () => {
+  it('minMax', () => {
+    expect(minMax([1, 2])).to.deep.equal([1, 2]);
+    expect(minMax([1, 2, 3, 4, 5])).to.deep.equal([1, 5]);
+    expect(minMax([[2334454, 5]])).to.have.ordered.members([5, 2334454])
+      .but.not.have.ordered.members([2334454, 5]);
+  });
+});
