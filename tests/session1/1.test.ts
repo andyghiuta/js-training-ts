f07e85132108294afacbeabee5ec7eb941f5c8a5
Andy Ghiuta <aghiuta@ena.com>
2020-07-02 12:06:51 +0300

    Fix session 1 tests

--- tests/session1/1.test.ts
+++ tests/session1/1.test.ts
@@ -4,14 +4,18 @@ import { sum, animals } from '../../session1/coding-challenges/1';
 describe('Session 1', () => {
   it('calculates sum', () => {
     expect(sum(1, 2)).to.equal(3);
-    expect(sum('1', 2)).to.equal(3);
-    expect(sum(1, '2')).to.equal(3);
+    // no need for the following two if covered by TypeScript (type definitions)
+    // expect(sum('1', 2)).to.equal(3);
+    // expect(sum(1, '2')).to.equal(3);
     expect(sum(-1, 2)).to.equal(1);
   });
   it('calculates legs', () => {
     expect(animals(1, 2, 3)).to.equal(22);
-    expect(animals(2, 3, 5)).equal(36);
-    expect(animals(5, 2, 8)).equal(50);
-    expect(animals(-1, 2, 3)).to.throw();
+    expect(animals(2, 3, 5)).to.equal(36);
+    expect(animals(5, 2, 8)).to.equal(50);
+    const getWrongAnimalLegs = () => {
+      animals(-1, 2, 3);
+    };
+    expect(getWrongAnimalLegs).to.throw(/wrong type/);
   });
 });
--- tests/session1/2.test.ts
+++ tests/session1/2.test.ts
@@ -5,7 +5,8 @@ describe('Session 1', () => {
   it('minMax', () => {
     expect(minMax([1, 2])).to.deep.equal([1, 2]);
     expect(minMax([1, 2, 3, 4, 5])).to.deep.equal([1, 5]);
-    expect(minMax([[2334454, 5]])).to.have.ordered.members([5, 2334454])
+    expect(minMax([1, 2, -3, 4, 5])).to.deep.equal([-3, 5]);
+    expect(minMax([2334454, 5])).to.have.ordered.members([5, 2334454])
       .but.not.have.ordered.members([2334454, 5]);
   });
 });
